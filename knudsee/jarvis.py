f7bc0ec7ec5049eae502152d2678dce75bf767e0
eianknudsen <eianknudsenn@gmail.com>
2022-02-05 14:01:30 -0500

    Working Window

--- knudsee/jarvis.py
+++ knudsee/jarvis.py
@@ -1,1 +1,13 @@
-#Test
+#import jarvis
+from tkinter import *
+
+window = Tk()
+
+#Creating Label
+testLabel = Label(window, text="Hello world! This is a test GUI.")
+
+#Puts label onto the window
+testLabel.pack()
+
+#Loops through window realtime
+window.mainloop()
