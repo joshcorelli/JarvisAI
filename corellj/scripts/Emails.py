352aa3539177f3b47a63398f67d7ca93c35c3ffc
joshcorelli <65784335+joshcorelli@users.noreply.github.com>
2022-04-26 22:17:26 -0400

    deleted folder

--- corellj/scripts/Emails.py
+++
@@ -1,2 +0,0 @@
-emailList = {"1": "timcorelli@yahoo.com", "2": "joshuacorelli@gmail.com", 
-"3": "corellj@sunypoly.edu", "4": "someone@gmail.com", "5": "knudsee@sunypoly.edu", "6": "mcalyssa01@gmail.com"}
